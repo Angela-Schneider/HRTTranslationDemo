69bb41cfa67c21f022f1376f5bd516c3dd46b01d
Gysau <64136624+Gysau@users.noreply.github.com>
2020-11-21 21:06:43 +0100

    +

--- src/detect-language.js
+++ src/detect-language.js
@@ -1,15 +1,16 @@
 const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
-
+/*
 const languageTranslator = new LanguageTranslatorV3({
   version: '2018-05-01',
   authenticator: new IamAuthenticator({
     apikey: '{apikey}',
   }),
   serviceUrl: '{url}',
-});
 
-console.log('found stuff, doing stuff');
+});
+*/
+
 
 /**
  * Helper 
